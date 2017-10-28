06c6e6b1d5ae3c8713712a78e64cd91d9e3a02d9
c0rnh0li0 <darkokrstev@gmail.com>
2017-10-28 19:58:46 +0200

    First commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+## Mandec CMS
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/** CATCH-ALL ROUTE for Backpack/PageManager - needs to be at the end of your routes.php file  **/
+Route::get('{page}/{subs?}', ['uses' => 'PageController@index'])
+    ->where(['page' => '^((?!admin).)*$', 'subs' => '.*']);
+
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth'], 'namespace' => 'Admin'], function () {
+    // Backpack\NewsCRUD
+    //CRUD::resource('article', 'ArticleCrudController');
+    //CRUD::resource('category', 'CategoryCrudController');
+    //CRUD::resource('tag', 'TagCrudController');
+});
